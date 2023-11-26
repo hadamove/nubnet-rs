be65cf1dd8dd77cd8789b9fa8480945dd089e914
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-11-26 11:24:59 +0100

    refact: naming

--- src/activators.rs
+++ src/activators.rs
@@ -1,6 +1,6 @@
 pub enum Activator {
     Identity,
-    ReLU,
+    Relu,
     Tanh,
     Softmax,
 }
@@ -9,7 +9,7 @@ impl Activator {
     pub fn apply(&self, values: &mut [f64]) {
         match self {
             Activator::Identity => {}
-            Activator::ReLU => values.iter_mut().for_each(|x| *x = x.max(0.0)),
+            Activator::Relu => values.iter_mut().for_each(|x| *x = x.max(0.0)),
             Activator::Tanh => values.iter_mut().for_each(|x| *x = x.tanh()),
             Activator::Softmax => {
                 let exp_sum: f64 = values.iter().map(|&x| x.exp()).sum();
@@ -21,7 +21,7 @@ impl Activator {
     pub fn apply_prime(&self, values: &mut [f64]) {
         match self {
             Activator::Identity => {}
-            Activator::ReLU => values.iter_mut().for_each(|x| *x = (*x > 0.0) as u8 as f64),
+            Activator::Relu => values.iter_mut().for_each(|x| *x = (*x > 0.0) as u8 as f64),
             Activator::Tanh => values.iter_mut().for_each(|x| *x = 1.0 - x.tanh().powi(2)),
             Activator::Softmax => {}
         }
--- src/layer.rs
+++ src/layer.rs
@@ -1,22 +1,21 @@
 use crate::activators::Activator;
 use rand::Rng;
 
-pub(crate) struct Layer {
-    pub(crate) size: usize,
-    pub(crate) potential: Vec<f64>,
-    pub(crate) activation: Vec<f64>,
-    pub(crate) delta: Vec<f64>,
-    pub(crate) inbound_weights: Vec<Vec<f64>>,
-    pub(crate) activator: Activator,
+pub struct Layer {
+    pub size: usize,
+    pub potential: Vec<f64>,
+    pub activation: Vec<f64>,
+    pub delta: Vec<f64>,
+    pub inbound_weights: Vec<Vec<f64>>,
+    pub activator: Activator,
 }
 
 impl Layer {
-    pub(crate) fn new(size: usize, input_size: usize, activator: Activator) -> Self {
+    pub fn new(size: usize, input_size: usize, activator: Activator) -> Self {
+        let size_with_bias = size + 1;
+
         let mut thread_rng = rand::thread_rng();
-
-        let size_with_bias = size + 1; // +1 for bias neuron
-
-        let input_weights: Vec<Vec<f64>> = (0..size_with_bias)
+        let inbound_weights: Vec<Vec<f64>> = (0..size_with_bias)
             .map(|_| {
                 (0..input_size)
                     .map(|_| thread_rng.gen::<f64>() / input_size as f64)
@@ -29,7 +28,7 @@ impl Layer {
             potential: vec![1.0; size_with_bias],
             activation: vec![1.0; size_with_bias],
             delta: vec![0.0; size_with_bias],
-            inbound_weights: input_weights,
+            inbound_weights,
             activator,
         }
     }
