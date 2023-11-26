e80aff436513f4cc2be12be0f47618afb6668426
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-11-26 20:55:59 +0100

    feat: add run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-## change this file to your needs
 
-echo "Adding some modules"
-
-# module add gcc-10.2
-
+module add rustup-1.24.3
+rustup default 1.74.0
 
 echo "#################"
 echo "    COMPILING    "
 echo "#################"
 
-## dont forget to use comiler optimizations (e.g. -O3 or -Ofast)
-# g++ -Wall -std=c++17 -O3 src/main.cpp src/file2.cpp -o network
-
+cargo build --release
 
 echo "#################"
 echo "     RUNNING     "
 echo "#################"
 
-## use nice to decrease priority in order to comply with aisa rules
-## https://www.fi.muni.cz/tech/unix/computation.html.en
-## especially if you are using multiple cores
-# nice -n 19 ./network
+nice -n 19 ./target/release/train_mnist
