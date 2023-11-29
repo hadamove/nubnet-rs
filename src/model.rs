e9e31d7a7684f41a4f55d5fdc4938714b9c5aeda
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-11-29 22:12:46 +0100

    refact: model

--- src/model.rs
+++ src/model.rs
@@ -2,12 +2,147 @@ use std::sync::{mpsc, Arc, Barrier, RwLock};
 
 use crate::{activators::Activator, layer::Layer};
 
-// TODO: Better naming
-struct ThreadState {
+pub struct Model<const N_THREADS: usize> {
+    state: ModelState,
+    barrier: Arc<Barrier>,
+    threads: Vec<ThreadHandle>,
+    learning_rate: f64,
+}
+
+impl<const N_THREADS: usize> Model<N_THREADS> {
+    /// Initializes the model with `N_THREADS` child threads and a learning rate.
+    pub fn new(learning_rate: f64) -> Self {
+        // Create a barrier synchronizes N_THREADS child threads and the main thread
+        // The FW and BW pass is done in the child threads, the weight update is done in the main thread
+        let barrier = Arc::new(Barrier::new(N_THREADS + 1));
+
+        // Spawn N_THREADS threads for computing the FW and BW pass
+        let threads = (0..N_THREADS)
+            .map(|_| {
+                let barrier = barrier.clone();
+                // Create a channel for sending inputs and labels from the main thread to the child threads
+                let (sender, receiver) = mpsc::channel();
+
+                // One copy of the pointer to the state for the main thread, one for the child thread
+                let state = Arc::new(RwLock::new(ModelState { layers: Vec::new() }));
+                let state_clone = state.clone();
+
+                std::thread::spawn(move || {
+                    Self::run_thread(state_clone, receiver, barrier);
+                });
+
+                ThreadHandle { state, sender }
+            })
+            .collect();
+
+        Self {
+            state: ModelState { layers: Vec::new() },
+            barrier,
+            threads,
+            learning_rate,
+        }
+    }
+
+    /// Adds a new layer to the model. The layer is added to all child threads states and the main thread state.
+    pub fn with_layer(mut self, size: usize, activator: Activator) -> Self {
+        // The input size of the new layer is the size of the last layer, or 0 if there are no layers yet (input layer)
+        let input_size = self.state.layers.last().map(|layer| layer.size).unwrap_or(0);
+
+        let new_layer = Layer::new(size, input_size, activator);
+        for thread in self.threads.iter_mut() {
+            thread.state.try_write().unwrap().layers.push(new_layer.clone());
+        }
+
+        self.state.layers.push(new_layer);
+
+        self
+    }
+
+    /// The inputs have to be `Arc<Vec<f64>>` so that they can be sent to the child threads safely and without cloning.
+    pub fn train_on_batch(&mut self, inputs: &[InputData], labels: &[InputData]) {
+        // Send inputs and labels to all threads
+        for ((input, desired), thread) in inputs.iter().zip(labels).zip(&self.threads) {
+            thread.sender.send((input.clone(), desired.clone())).unwrap();
+        }
+
+        // Wait for all child threads to finish forward and backward pass
+        self.barrier.wait();
+
+        // Update weights
+        self.update_weights();
+    }
+
+    /// Predicts the output for the given input. The input slice must be of the same size as the first layer.
+    pub fn predict(&mut self, input: &[f64]) -> &[f64] {
+        // Run the forward pass on the main thread
+        self.state.feed_forward(input);
+        self.get_output()
+    }
+
+    fn run_thread(
+        state: Arc<RwLock<ModelState>>,
+        receiver: mpsc::Receiver<(InputData, InputData)>,
+        barrier: Arc<Barrier>,
+    ) {
+        loop {
+            // receiver.recv() blocks this thread until main thread feeds us more data
+            let Ok((input, desired)) = receiver.recv() else {
+                // The main thread has dropped the sender, so we can exit
+                break;
+            };
+
+            // Acquire lock to state and feed forward and backward
+            {
+                let mut state = state.try_write().unwrap();
+                state.feed_forward(&input);
+                state.feed_backward(&desired);
+            }
+            // End of scope -> lock is released
+
+            // Wait for all threads to finish forward and backward pass
+            barrier.wait();
+        }
+    }
+
+    fn update_weights(&mut self) {
+        for thread in self.threads.iter_mut() {
+            // It is safe to aquire the lock here, the child threads are waiting for the next input
+            let mut state = thread.state.try_write().unwrap();
+
+            state.layers.iter_mut().reduce(|layer_below, layer| {
+                {
+                    let mut inbound_weights = layer.inbound_weights.try_write().unwrap();
+
+                    inbound_weights.iter_mut().zip(0..).for_each(|(weights, i)| {
+                        weights.iter_mut().zip(0..layer_below.size).for_each(|(weight, j)| {
+                            *weight += self.learning_rate * layer.delta[i] * layer_below.activation[j]
+                        });
+                    });
+                }
+
+                layer
+            });
+        }
+    }
+
+    fn get_output(&self) -> &[f64] {
+        &self.state.layers.last().expect("No layers in the model").activation[1..]
+    }
+}
+
+type InputData = Arc<Vec<f64>>;
+
+// Holds the state of a child thread
+struct ThreadHandle {
+    state: Arc<RwLock<ModelState>>,
+    sender: mpsc::Sender<(InputData, InputData)>,
+}
+
+struct ModelState {
     layers: Vec<Layer>,
 }
 
-impl ThreadState {
+impl ModelState {
     fn feed_forward(&mut self, input: &[f64]) {
         self.layers[0].activation[1..].copy_from_slice(input);
 
@@ -49,143 +184,6 @@ impl ThreadState {
     }
 }
 
-type InputData = Arc<Vec<f64>>;
-
-pub struct Model<const N: usize> {
-    // TODO: Better naming
-    main_state: ThreadState,
-    barrier: Arc<Barrier>,
-    // TODO: Better naming
-    threads: Vec<Arc<RwLock<ThreadState>>>,
-    channels: Vec<mpsc::Sender<(InputData, InputData)>>,
-    learning_rate: f64,
-}
-
-fn thread_lifecycle(
-    thread_state: Arc<RwLock<ThreadState>>,
-    receiver: mpsc::Receiver<(InputData, InputData)>,
-    barrier: Arc<Barrier>,
-) {
-    loop {
-        // receiver.recv() blocks until main thread feeds us more data
-        // between the barrier.wait() and the receiver.recv() the main thread updates the weights
-        let Ok((input, desired)) = receiver.recv() else {
-            // The main thread has dropped the sender, so we can exit
-            break;
-        };
-
-        // Acquire lock to state and feed forward and backward
-        {
-            let mut thread_state = thread_state.try_write().unwrap();
-            thread_state.feed_forward(&input);
-            thread_state.feed_backward(&desired);
-        }
-        // End of scope -> lock is released
-
-        // Wait for all threads to finish forward and backward pass
-        barrier.wait();
-    }
-}
-
-impl<const N: usize> Default for Model<N> {
-    fn default() -> Self {
-        let barrier = Arc::new(Barrier::new(N + 1)); // shouldnt this be N+1?
-        let mut threads = Vec::new();
-        let mut channels = Vec::new();
-
-        for _ in 0..N {
-            let barrier = barrier.clone();
-            let thread_state = Arc::new(RwLock::new(ThreadState { layers: Vec::new() }));
-            let (sender, receiver) = mpsc::channel();
-
-            threads.push(thread_state.clone());
-            channels.push(sender);
-
-            std::thread::spawn(move || {
-                thread_lifecycle(thread_state, receiver, barrier);
-            });
-        }
-
-        Self {
-            main_state: ThreadState { layers: Vec::new() },
-            barrier,
-            threads,
-            channels,
-            learning_rate: 0.1,
-        }
-    }
-}
-
-impl<const N: usize> Model<N> {
-    pub fn with_layer(mut self, size: usize, activator: Activator) -> Self {
-        let input_size = self.main_state.layers.last().map(|layer| layer.size).unwrap_or(N);
-
-        let new_layer = Layer::new(size, input_size, activator);
-        for thread in self.threads.iter_mut() {
-            thread.try_write().unwrap().layers.push(new_layer.clone());
-        }
-
-        self.main_state.layers.push(new_layer);
-
-        self
-    }
-
-    pub fn with_learning_rate(mut self, learning_rate: f64) -> Self {
-        self.learning_rate = learning_rate;
-        self
-    }
-
-    /// The inputs have to be `Arc<Vec<f64>>` so that they can be sent to the child threads safely and without cloning.
-    pub fn train_on_batch(&mut self, inputs: &[InputData], labels: &[InputData]) {
-        // Send inputs and labels to all threads
-        for ((input, desired), thread_channel) in inputs.iter().zip(labels).zip(&self.channels) {
-            thread_channel.send((input.clone(), desired.clone())).unwrap();
-        }
-
-        // Wait for all child threads to finish forward and backward pass
-        self.barrier.wait();
-
-        // Update weights
-        self.update_weights();
-    }
-
-    /// Predicts the output for the given input. The input slice must be of the same size as the first layer.
-    pub fn predict(&mut self, input: &[f64]) -> &[f64] {
-        // Run the forward pass on the first thread
-        self.main_state.feed_forward(input);
-        self.get_output()
-    }
-
-    fn update_weights(&mut self) {
-        for thread_state in self.threads.iter_mut() {
-            // It should be safe to aquire the lock here, since only the main thread is running, and the child threads are waiting for the next input
-            let mut thread_state = thread_state.try_write().unwrap();
-
-            thread_state.layers.iter_mut().reduce(|layer_below, layer| {
-                {
-                    let mut inbound_weights = layer.inbound_weights.try_write().unwrap();
-                    inbound_weights.iter_mut().zip(0..).for_each(|(weights, i)| {
-                        weights.iter_mut().zip(0..layer_below.size).for_each(|(weight, j)| {
-                            *weight += self.learning_rate * layer.delta[i] * layer_below.activation[j]
-                        });
-                    });
-                }
-
-                layer
-            });
-        }
-    }
-
-    fn get_output(&self) -> &[f64] {
-        &self
-            .main_state
-            .layers
-            .last()
-            .expect("No layers in the model")
-            .activation[1..]
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::sync::Arc;
@@ -196,11 +194,10 @@ mod tests {
 
     #[test]
     fn test_xor() {
-        let mut network = Model::<NUM_THREADS>::default()
+        let mut network = Model::<NUM_THREADS>::new(0.03)
             .with_layer(2, Activator::Identity)
             .with_layer(2, Activator::Tanh)
-            .with_layer(1, Activator::Identity)
-            .with_learning_rate(0.03);
+            .with_layer(1, Activator::Identity);
 
         let data = [
             Arc::new(vec![0., 0.]),
