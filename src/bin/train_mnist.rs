342186093b64aa81fc6bdfff2dd4885ff3f7dc17
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-11-29 22:50:55 +0100

    refact: train mnist

--- src/bin/train_mnist.rs
+++ src/bin/train_mnist.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 
 use pv021_project::{activators::Activator, model::Model};
 
-const NUM_THREADS: usize = 1;
+const NUM_THREADS: usize = 8;
 
 const NUM_CLASSES: usize = 10;
 
@@ -18,41 +18,33 @@ const TRAIN_LABELS_PATH: &str = "data/fashion_mnist_train_labels.csv";
 const TRAIN_PREDICTIONS_PATH: &str = "train_predictions.csv";
 const TEST_PREDICTIONS_PATH: &str = "test_predictions.csv";
 
-const LEARNING_RATE: f64 = 0.001;
+const LEARNING_RATE: f64 = 0.0005;
 const NUM_EPOCHS: usize = 20;
 
 // Run with `cargo run --release` to get reasonable performance
 fn main() -> Result<()> {
     let train_data = load_vectors(TRAIN_VECTORS_PATH)?;
-    // TODO: Refactor this mess
-    let train_data: Vec<_> = train_data.into_iter().map(Arc::new).collect();
-
     let train_labels = load_labels(TRAIN_LABELS_PATH)?;
-    // TODO: Refactor this mess
-    let train_labels: Vec<_> = train_labels.into_iter().map(label_to_one_hot).map(Arc::new).collect();
 
     let test_data = load_vectors(TEST_VECTORS_PATH)?;
     let test_labels = load_labels(TEST_LABELS_PATH)?;
 
-    let mut model = Model::<NUM_THREADS>::default()
+    let mut model = Model::<NUM_THREADS>::new(LEARNING_RATE)
         .with_layer(784, Activator::Identity)
         .with_layer(128, Activator::Tanh)
         .with_layer(64, Activator::Tanh)
-        .with_layer(10, Activator::Softmax)
-        .with_learning_rate(LEARNING_RATE);
+        .with_layer(10, Activator::Softmax);
 
     let t0 = std::time::Instant::now();
 
     for epoch in 0..NUM_EPOCHS {
-        // TODO: Refactor this (use `step_by`)
-        for batch in 0..(train_data.len() / NUM_THREADS) {
-            let batch_start = batch * NUM_THREADS;
-            let batch_end = batch_start + NUM_THREADS;
+        for batch_start in (0..train_data.len()).step_by(NUM_THREADS) {
+            let batch_end = usize::min(batch_start + NUM_THREADS, train_data.len());
 
-            let batch_data = &train_data[batch_start..batch_end];
-            let batch_labels = &train_labels[batch_start..batch_end];
+            let inputs = &train_data[batch_start..batch_end];
+            let labels = &train_labels[batch_start..batch_end];
 
-            model.train_on_batch(batch_data, batch_labels);
+            model.train_on_batch(inputs, labels);
         }
 
         // TODO: Remove this scope before submitting (>Any implementation that uses testing input vectors for anything else than final evaluation will result in failure)
@@ -68,14 +60,13 @@ fn main() -> Result<()> {
         }
     }
 
-    // TODO: Uncomment and fix parameters
-    // export_predictions(&mut model, &train_data, TRAIN_PREDICTIONS_PATH)?;
-    // export_predictions(&mut model, &test_data, TEST_PREDICTIONS_PATH)?;
+    export_predictions(&mut model, &train_data, TRAIN_PREDICTIONS_PATH)?;
+    export_predictions(&mut model, &test_data, TEST_PREDICTIONS_PATH)?;
 
     Ok(())
 }
 
-fn load_vectors(filename: &str) -> Result<Vec<Vec<f64>>> {
+fn load_vectors(filename: &str) -> Result<Vec<Arc<Vec<f64>>>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
@@ -90,10 +81,11 @@ fn load_vectors(filename: &str) -> Result<Vec<Vec<f64>>> {
         })
         .collect();
 
-    vectors
+    // We wrap the vectors in Arc so that they can be shared between threads by pointer without cloning
+    vectors.map(|v| v.into_iter().map(Arc::new).collect::<Vec<_>>())
 }
 
-fn load_labels(filename: &str) -> Result<Vec<usize>> {
+fn load_labels(filename: &str) -> Result<Vec<Arc<Vec<f64>>>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
@@ -103,10 +95,11 @@ fn load_labels(filename: &str) -> Result<Vec<usize>> {
         .map(|line| line.parse::<usize>().map_err(Into::into))
         .collect();
 
-    labels
+    // We convert the labels to one hot vectors, then we wrap them in Arc similarly to the vectors
+    labels.map(|v| v.into_iter().map(label_to_one_hot).map(Arc::new).collect::<Vec<_>>())
 }
 
-fn export_predictions(model: &mut Model<NUM_THREADS>, data: &[Vec<f64>], filename: &str) -> Result<()> {
+fn export_predictions(model: &mut Model<NUM_THREADS>, data: &[Arc<Vec<f64>>], filename: &str) -> Result<()> {
     let mut file = File::create(filename)?;
 
     for input in data {
@@ -118,13 +111,13 @@ fn export_predictions(model: &mut Model<NUM_THREADS>, data: &[Vec<f64>], filenam
     Ok(())
 }
 
-fn calculate_accuracy(model: &mut Model<NUM_THREADS>, data: &[Vec<f64>], labels: &[usize]) -> f64 {
+fn calculate_accuracy(model: &mut Model<NUM_THREADS>, data: &[Arc<Vec<f64>>], labels: &[Arc<Vec<f64>>]) -> f64 {
     let correct = data
         .iter()
         .zip(labels)
         .filter(|(data, label)| {
             let prediction = model.predict(data);
-            one_hot_to_label(prediction) == **label
+            one_hot_to_label(prediction) == one_hot_to_label(label)
         })
         .count();
 
