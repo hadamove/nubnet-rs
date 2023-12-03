2ccfcbe79a06a2a11a4bf93b580e0b2ff56aaa56
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-12-03 18:06:30 +0100

    remove unused activator

--- src/activators.rs
+++ src/activators.rs
@@ -1,7 +1,6 @@
 #[derive(Clone)]
 pub enum Activator {
     Identity,
-    Relu,
     Tanh,
     Softmax,
 }
@@ -10,7 +9,6 @@ impl Activator {
     pub fn apply(&self, values: &mut [f64]) {
         match self {
             Activator::Identity => {}
-            Activator::Relu => values.iter_mut().for_each(|x| *x = x.max(0.0)),
             Activator::Tanh => values.iter_mut().for_each(|x| *x = x.tanh()),
             Activator::Softmax => {
                 let exp_sum: f64 = values.iter().map(|&x| x.exp()).sum();
@@ -22,7 +20,6 @@ impl Activator {
     pub fn apply_prime(&self, values: &mut [f64]) {
         match self {
             Activator::Identity => {}
-            Activator::Relu => values.iter_mut().for_each(|x| *x = (*x > 0.0) as u8 as f64),
             Activator::Tanh => values.iter_mut().for_each(|x| *x = 1.0 - x.tanh().powi(2)),
             Activator::Softmax => {}
         }
